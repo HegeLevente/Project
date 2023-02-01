01b95a908afa7a501021200e32aa43a657ada007
HegeLevente <hegelevente0712@gmail.com>
2023-02-01 09:31:26 +0100

    bootstrap-el

--- webProject0_HJL/app.js
+++ webProject0_HJL/app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist')));
 
 
 
